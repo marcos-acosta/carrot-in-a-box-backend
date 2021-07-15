23ba4588c6d699702c5dc0b810b1bf843907fb69
Marcos Acosta <macosta1625@gmail.com>
2021-07-15 13:29:44 -0400

    Maybe a slash will fix it?

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = app.listen(port, () => {
 const io = socketIO(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "https://carrot-in-a-box.netlify.app/",
+    origin: "https://carrot-in-a-box.netlify.app",
     credentials: true
   }
 });
